24c92d4337ad1b62cba2ad8c89d58cceaa7b57e8
wonderm-swift@hotmail.com <wonderm-swift@hotmail.com>
2020-02-04 19:02:30 +0800

    重构插件结构

---
+++ Assets/ExcelToSqliteConverter/Tests/Editor/Test.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using Wonderm.SQLiteNs;
+
+public class Test
+{
+    [MenuItem("Tool/Excel/Test &1")]
+    private static void ShowXlS()
+    {
+        string path = Application.streamingAssetsPath + "/Test/Test3.xlsx";
+
+        ExcelToSQLite.ConvertToSQLite(path);
+    }
+
+    private static string SqlitePath
+    {
+        get
+        {
+            return Application.streamingAssetsPath + "/DataBase/Data.db";
+        }
+    }
+
+    [MenuItem("Tool/Excel/Test &2")]
+    private static void ReadSqlite()
+    {
+        var db = new SQLiteHelper(SqlitePath, false);
+
+        var datas = DbReader.Read<Test2Data>(db);
+
+        if (datas == null) return;
+
+        foreach (var d in datas)
+        {
+            Debug.Log(JsonUtility.ToJson(d));
+        }
+    }
+}

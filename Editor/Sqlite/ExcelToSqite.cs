54209f2a2b951bd78ad976c6212652f98e80a143
wonderm-swift@hotmail.com <wonderm-swift@hotmail.com>
2020-02-04 19:06:25 +0800

    修改文件路径

---
+++ Editor/Sqlite/ExcelToSqite.cs
@@ -0,0 +1,141 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+using Wonderm.SQLiteNs;
+using Wonderm.ExcelNs;
+
+namespace Wonderm.SQLiteNs
+{
+    public class ExcelToSQLite
+    {
+        /// <summary>
+        /// 导出表名的后缀
+        /// </summary>
+        private const string ExportExtra = "_Export";
+
+        private string defaultSqlitePath = Application.streamingAssetsPath + "/DataBase/Data.db";
+
+        private static string sqlitePath;
+
+        public static string SqlitePath
+        {
+            get
+            {
+                return sqlitePath;
+            }
+        }
+
+        private static List<string> TypeLimits = new List<string> {
+        "INTEGER",
+        "TEXT",
+        "REAL",
+        "BLOB",
+    };
+
+        private ExcelToSQLite()
+        {
+            sqlitePath = defaultSqlitePath;
+        }
+
+        public static void SetSqlitePath(string filePath)
+        {
+            sqlitePath = filePath;
+        }
+
+        public static void ConvertToSQLite(string excelPath)
+        {
+            var excel = ExcelHelper.LoadExcel(excelPath);
+            if (excel == null) return;
+
+            ExcelToSQLite.ConvertToSQLite(excel);
+
+            UnityEditor.AssetDatabase.Refresh();
+        }
+
+        public static void ConvertToSQLite(Excel excel)
+        {
+            if (File.Exists(SqlitePath))
+            {
+                File.Delete(SqlitePath);
+            }
+
+            SQLiteHelper.CreateSQLiteDB(SqlitePath);
+
+            using (var db = new SQLiteHelper(SqlitePath, true))
+            {
+                Convert(db, excel);
+            }
+        }
+
+        public static void Convert(SQLiteHelper db, Excel excel)
+        {
+            List<string> colNames = new List<string>();
+            List<string> coltypes = new List<string>();
+
+            foreach (var table in excel.Tables)
+            {
+                if (!table.TableName.EndsWith(ExportExtra)) continue;
+
+                #region 创建表
+
+                if (table.NumberOfRows < 2)
+                {
+                    Debug.LogError("Data format error : NumberOfRows is less than 2");
+                }
+                string tableName = table.TableName.Replace(ExportExtra, "");
+
+                {
+                    for (int j = 1; j <= table.NumberOfColumns; j++)
+                    {
+                        if (colNames.Contains(table.GetCell(1, j).Value))
+                        {
+                            Debug.LogError(string.Format("tableName [{0}] repeated in table [{1}]", table.GetCell(1, j).Value, table.TableName));
+                            return;
+                        }
+
+                        colNames.Add(table.GetCell(1, j).Value);
+                        var cel = table.GetCell(2, j).Value;
+                        if (!TypeLimits.Contains(cel))
+                        {
+                            Debug.LogError(string.Format("Type error in table [{0}] in ({1},{2}) error type {3}", tableName, 2, j, cel));
+                            return;
+                        }
+
+                        coltypes.Add(cel);
+                    }
+
+                    if (colNames.Count != coltypes.Count)
+                    {
+                        Debug.LogError("ColNames do not match ColTypes in count : " + table.TableName);
+                    }
+
+                    db.CreateTable(tableName, colNames.ToArray(), coltypes.ToArray());
+                }
+
+                #endregion 创建表
+
+                #region 插入数据
+
+                for (int i = 3; i <= table.NumberOfRows; i++)
+                {
+                    List<string> cols = new List<string>();
+
+                    for (int j = 1; j <= table.NumberOfColumns; j++)
+                    {
+                        cols.Add(table.GetCell(i, j).Value);
+                    }
+
+                    if (colNames.Count != cols.Count)
+                    {
+                        Debug.LogError("ColNames do not match Cols in count : " + table.TableName);
+                        return;
+                    }
+
+                    db.InsertValues(tableName, cols.ToArray());
+                }
+
+                #endregion 插入数据
+            }
+        }
+    }
+}
---
+++ Runtime/DbReader/DbReader.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+namespace Wonderm.SQLiteNs
+{
+    public static class DbReader
+    {
+        [AttributeUsage (AttributeTargets.Class)]
+        public class DbTableAttribute : System.Attribute
+        {
+            public DbTableAttribute (string tableName)
+            {
+                TableName = tableName;
+            }
+
+            public string TableName;
+        }
+
+        [AttributeUsage (AttributeTargets.Property | AttributeTargets.Field)]
+        public class DbFieldAttribute : System.Attribute
+        {
+            public DbFieldAttribute (string fieldName, char separator = ',')
+            {
+                FieldName = fieldName;
+
+                Separator = separator;
+            }
+
+            public string FieldName;
+
+            public char Separator;
+        }
+
+        #region Attribute
+
+        private static FieldInfo[] GetFieldInfos (System.Type t)
+        {
+            return t.GetFields (BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        }
+
+        private static PropertyInfo[] GetPropertyInfos (System.Type t)
+        {
+            return t.GetProperties (BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+        }
+
+        private static DbTableAttribute GetDbTableAttribute (Type type)
+        {
+            if (!type.IsDefined (typeof (DbTableAttribute), true)) return null;
+
+            return type.GetCustomAttributes (typeof (DbTableAttribute), true) [0] as DbTableAttribute;
+        }
+
+        private static DbFieldAttribute GetDbFieldAttribute (MemberInfo memInfo)
+        {
+            if (!memInfo.IsDefined (typeof (DbFieldAttribute), true)) return null;
+
+            return memInfo.GetCustomAttributes (typeof (DbFieldAttribute), true) [0] as DbFieldAttribute;
+        }
+
+        private static MemberInfo[] GetFieldProperty<T> () where T : class, new ()
+        {
+            System.Type t = typeof (T);
+
+            MemberInfo[] mems = t.GetMembers (BindingFlags.Instance |
+                BindingFlags.NonPublic |
+                BindingFlags.Public
+            );
+
+            return mems;
+        }
+
+        #endregion Attribute
+
+        private static object GetValue (SQLiteDataReader reader, System.Type fieldType, DbFieldAttribute attr)
+        {
+            object v = null;
+
+            if (fieldType == typeof (int))
+            {
+                v = Converter.ToInt (reader, attr.FieldName);
+            }
+            else if (fieldType == typeof (byte))
+            {
+                v = (byte) Converter.ToInt (reader, attr.FieldName);
+            }
+            else if (fieldType == typeof (float))
+            {
+                v = Converter.ToFloat (reader, attr.FieldName);
+            }
+            else if (fieldType == typeof (string))
+            {
+                v = Converter.ToString (reader, attr.FieldName);
+            }
+            else if (fieldType == typeof (bool))
+            {
+                v = Converter.ToBool (reader, attr.FieldName);
+            }
+            else if (fieldType == typeof (Vector3))
+            {
+                v = Converter.ToVector3 (reader, attr.FieldName, attr.Separator);
+            }
+            else if (fieldType == typeof (Vector2))
+            {
+                v = Converter.ToVector2 (reader, attr.FieldName, attr.Separator);
+            }
+            else if (fieldType.IsEnum)
+            {
+                v = System.Enum.Parse (fieldType, Converter.ToString (reader, attr.FieldName));
+            }
+            else if (fieldType == typeof (int[]))
+            {
+                v = Converter.GetIntArray (reader, attr.FieldName, attr.Separator);
+            }
+            else if (fieldType == typeof (float[]))
+            {
+                v = Converter.ToFloatArray (reader, attr.FieldName, attr.Separator);
+            }
+            else if (fieldType == typeof (string[]))
+            {
+                v = Converter.ToStringArray (reader, attr.FieldName, attr.Separator);
+            }
+            else
+            {
+                Debug.LogError ("not supported value type:" + fieldType);
+            }
+            return v;
+        }
+
+        private static int attrTEMP;
+        private static List<DbFieldAttribute> dbFieldAttrTEMP;
+        private static List<DbFieldAttribute> dbPropertyAttrTEMP;
+        private static T BuildListItem<T> (SQLiteDataReader reader, FieldInfo[] fieldInfos, PropertyInfo[] propertyInfos, int cacheId = 0) where T : class, new ()
+        {
+            if (cacheId == 0 || cacheId != attrTEMP)
+            {
+                dbFieldAttrTEMP = null;
+                dbPropertyAttrTEMP = null;
+            }
+
+            attrTEMP = cacheId;
+
+            T item = new T ();
+
+            for (int i = 0; i < fieldInfos.Length; i++)
+            {
+                var field = fieldInfos[i];
+                if (dbFieldAttrTEMP == null)
+                {
+                    dbFieldAttrTEMP = new List<DbFieldAttribute> (8);
+                }
+
+                if (dbFieldAttrTEMP.Count < fieldInfos.Length)
+                {
+                    DbFieldAttribute dbFieldAttr = GetDbFieldAttribute (field);
+                    dbFieldAttrTEMP.Add (dbFieldAttr);
+                }
+                var atrr = dbFieldAttrTEMP[i];
+                if (atrr == null)
+                {
+                    continue;
+                }
+
+                object v = GetValue (reader, field.FieldType, atrr);
+
+                field.SetValue (item, v);
+            }
+
+            for (int i = 0; i < propertyInfos.Length; i++)
+            {
+                var property = propertyInfos[i];
+
+                if (dbPropertyAttrTEMP == null)
+                {
+                    dbPropertyAttrTEMP = new List<DbFieldAttribute> (8);
+                }
+
+                if (dbPropertyAttrTEMP.Count < fieldInfos.Length)
+                {
+                    DbFieldAttribute dbPropertyttr = GetDbFieldAttribute (property);
+                    dbPropertyAttrTEMP.Add (dbPropertyttr);
+                }
+                var atrr = dbPropertyAttrTEMP[i];
+                if (atrr == null)
+                {
+                    continue;
+                }
+                object v = GetValue (reader, property.PropertyType, atrr);
+
+                property.SetValue (item, v, null);
+            }
+
+            return item;
+        }
+
+        private static T BuildItem<T> (SQLiteDataReader reader, FieldInfo[] fieldInfos, PropertyInfo[] propertyInfos) where T : class, new ()
+        {
+            T item = new T ();
+
+            foreach (FieldInfo field in fieldInfos)
+            {
+                DbFieldAttribute dbFieldAttr = GetDbFieldAttribute (field);
+                if (dbFieldAttr == null)
+                {
+                    continue;
+                }
+
+                object v = GetValue (reader, field.FieldType, dbFieldAttr);
+
+                field.SetValue (item, v);
+            }
+
+            foreach (PropertyInfo property in propertyInfos)
+            {
+                DbFieldAttribute dbPropertyAttr = GetDbFieldAttribute (property);
+                if (dbPropertyAttr == null)
+                {
+                    continue;
+                }
+                object v = GetValue (reader, property.PropertyType, dbPropertyAttr);
+
+                property.SetValue (item, v, null);
+            }
+
+            return item;
+        }
+
+        public static List<T> Read<T> (SQLiteHelper helper, int capacity = 20) where T : class, new ()
+        {
+            var table = GetDbTableAttribute (typeof (T));
+            if (table != null)
+            {
+                var reader = helper.ReadFullTable (table.TableName);
+                return Read<T> (reader, capacity);
+            }
+            Debug.LogError (string.Format ("Class {0} is do not have a attribute named DbTable", typeof (T).Name));
+            return null;
+        }
+
+        public static List<T> Read<T> (SQLiteDataReader reader, int capacity = 20) where T : class, new ()
+        {
+            System.Type t = typeof (T);
+            FieldInfo[] fieldInfos = GetFieldInfos (t);
+            PropertyInfo[] propertyInfos = GetPropertyInfos (t);
+
+            List<T> list = new List<T> (capacity);
+
+            int cacheId = reader.GetHashCode ();
+            
+            while (reader.Read ())
+            {
+                T item = BuildListItem<T> (reader, fieldInfos, propertyInfos, cacheId);
+                list.Add (item);
+            }
+
+            return list;
+        }
+
+        public static T ReadItem<T> (SQLiteDataReader reader) where T : class, new ()
+        {
+            System.Type t = typeof (T);
+            FieldInfo[] fieldInfos = GetFieldInfos (t);
+            PropertyInfo[] propertyInfos = GetPropertyInfos (t);
+
+            return BuildItem<T> (reader, fieldInfos, propertyInfos);
+        }
+    }
+}
